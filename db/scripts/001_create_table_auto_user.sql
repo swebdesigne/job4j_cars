236f3034b53d5aff396522ea573ec1f04232543f
Igor <swebdesigne@gmail.com>
2023-01-23 15:35:39 +0300

    2. Доменная модель проекта "АвтоМаг"

---
+++ db/scripts/001_create_table_auto_user.sql
@@ -0,0 +1,6 @@
+CREATE TABLE auto_user
+(
+    id       SERIAL PRIMARY KEY,
+    login    TEXt NOT NULL,
+    password TEXT NOT NULL
+)
---
+++ db/scripts/002_create_table_auto_post.sql
@@ -0,0 +1,7 @@
+CREATE TABLE auto_post
+(
+    id           INTEGER PRIMARY KEY,
+    description  VARCHAR,
+    created      TIMESTAMP,
+    auto_user_id INTEGER REFERENCES auto_post (id) UNIQUE
+)
